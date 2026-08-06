dc61b93dbe666c575b3dbd083ebe0a0ee4f482d5
Kacper <kgrzesik17@gmail.com>
2026-08-06 19:00:04 +0200

    basic form styling

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 "use strict";
 
 const isImperialSwitch = document.querySelector("#is-imperial");
+const submitButton = document.querySelector(".feature-submit");
+
+const heightInput = document.querySelector("#height");
+
+let height, weight, age, target_weight, time, isImperial, sex, activity;
 
 isImperialSwitch.addEventListener("click", function () {
   this.classList.toggle("switch__off");
 });
+
+submitButton.addEventListener("click", function () {
+  validate();
+});
+
+function validate() {
+  console.log(heightInput.value);
+}
