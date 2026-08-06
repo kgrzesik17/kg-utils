3275d4ab9b9f19eb107fb8f8a7b435d56eda3b5b
Kacper <kgrzesik17@gmail.com>
2026-08-06 22:37:51 +0200

    deficit counter logic

--- src/js/main.js
+++ src/js/main.js
@@ -18,12 +18,31 @@ form.addEventListener("submit", (e) => {
   data = Object.fromEntries(formData.entries());
 
   console.log(data);
-  console.log(calculateMetabolism(data));
+  calculateCalories();
 });
 
-function calculateMetabolism(data) {
+function calculateMetabolism() {
   if (data.sex === "male")
     return 66.5 + 13.75 * data.weight + 5.003 * data.height - 6.775 * data.age;
   else if (data.sex === "female")
     return 655.1 + 9.563 * data.weight + 1.85 * data.height - 4.676 * data.age;
+}
+
+function calculateCalories() {
+  const goalDifference = data.weight - data.targetWeight;
+  const calorieDeficitOverall = goalDifference * 9 * 1000;
+  const calorieDeficitPerDay = calorieDeficitOverall / data.time;
+  const activityNumber = (1.025 + data.activity * 0.175).toFixed(2);
+
+  const tdee = (calculateMetabolism() * activityNumber).toFixed(0);
+  const diet = (tdee - calorieDeficitPerDay).toFixed(0);
+
+  console.log(`Daily base metabolism: ${calculateMetabolism()} kcal`);
+  console.log(`With all daily activities, that comes to ${tdee} kcal`);
+  console.log(
+    `You need to keep ${calorieDeficitPerDay} calorie sulprus/deficit per day`,
+  );
+  console.log(
+    `That means you have to eat around ${diet} calories per day for ${data.time} days to reach the goal`,
+  );
 }
