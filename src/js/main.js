e4db1ec4bcd9444185e6b3e6b7d6ae1e09103491
Kacper <kgrzesik17@gmail.com>
2026-08-24 17:30:14 +0200

    added dynamic render for imperial, metric. added dynamic slider behavior

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,45 @@
 "use strict";
 
+const activityDescriptions = [
+  "Siedzący tryb życia, praca biurowa, brak regularnych treningów lub sporadyczne spacery.",
+  "Lekka aktywność fizyczna, praca siedząca + lekki trening / sport 1-3 razy w tygodniu.",
+  "Umiarkowana aktywność, praca stojąca/chodząca lub trening o średniej intensywności 3-5 razy w tygodniu.",
+  "Bardzo wysoka aktywność, ciężka praca fizyczna lub intensywne treningi 6-7 razy w tygodniu.",
+  "Ekstremalna aktywność, sport wyczynowy / zawodowy, codzienne bardzo ciężkie treningi lub ciężka praca fizyczna połączona z treningami.",
+];
+
 const form = document.querySelector("form");
 const isImperialSwitch = document.querySelector("#is-imperial");
 const outputContainer = document.querySelector(".output");
+const activitySlider = document.querySelector("#activity");
+const activityDescription = document.querySelector("#activity-description");
 
 let data;
 
+function init() {
+  const baseActivityLevel = 3;
+
+  activityDescription.textContent = activityDescriptions[baseActivityLevel - 1];
+  activitySlider.setAttribute("value", baseActivityLevel);
+}
+
 // change switch styling on click
 isImperialSwitch.addEventListener("click", function () {
+  const heightInput = document.querySelector("#height");
+  const weightInput = document.querySelector("#weight");
+
   this.classList.toggle("switch__off");
+
+  this.dataset.isImperial =
+    this.dataset.isImperial === "false" ? "true" : "false";
+
+  if (this.dataset.isImperial === "false") {
+    heightInput.setAttribute("placeholder", "Wzrost (cm)");
+    weightInput.setAttribute("placeholder", "Waga (kg)");
+  } else {
+    heightInput.setAttribute("placeholder", "Wzrost (ft, inch)");
+    weightInput.setAttribute("placeholder", "Waga (lbs)");
+  }
 });
 
 // get data from the form
@@ -17,9 +48,9 @@ form.addEventListener("submit", (e) => {
 
   const formData = new FormData(form);
   data = Object.fromEntries(formData.entries());
+  data.isImperial = isImperialSwitch.dataset.isImperial;
 
   console.log(data);
-  // calculateCalories();
 
   renderOutput(data);
 
@@ -29,12 +60,17 @@ form.addEventListener("submit", (e) => {
   });
 });
 
+// calculate the base metabolism value
 function calculateMetabolism() {
   if (data.sex === "male")
     return 66.5 + 13.75 * data.weight + 5.003 * data.height - 6.775 * data.age;
   else if (data.sex === "female")
     return 655.1 + 9.563 * data.weight + 1.85 * data.height - 4.676 * data.age;
 }
+
+activitySlider.addEventListener("input", function (e) {
+  activityDescription.textContent = activityDescriptions[e.target.value - 1];
+});
 
 function renderOutput() {
   const goalDifference = data.weight - data.targetWeight;
@@ -70,3 +106,5 @@ function renderOutput() {
   outputContainer.insertAdjacentHTML("beforeend", html);
   outputContainer.style.visibility = "visible";
 }
+
+init();
